9f03c88e7cd5692ebd429c3a55ee2e5b25df9159
Tatvam <td14@iitbbs.ac.in>
2018-01-22 23:38:02 +0530

    Changes

--- script1.js
+++ script1.js
@@ -1,17 +1,19 @@
-/*
+
 var height1=window.screen.availHeight+"px";
 function openmenu(){
-  document.getElementById("sidemenu").style.height = "100%";
-  document.getElementById("sidemenu").style.width = "50%";
- //document.getElementById("main").style.marginLeft = "100px";
- //document.getElementById("a1").style.marginLeft = "100px";
+  //document.getElementById("sidemenu1").style.display="grid";
+  document.getElementById("sidemenu1").style.visibility="visible";
   document.getElementById("get_list").style.visibility="hidden";
+  document.getElementById("sidemenu1").style.width="150px";
+  document.getElementById("sidemenu1").style.height="100%";
    }
+  
 function closebar(){
- document.getElementById("sidemenu").style.width="0";
- document.getElementById("main").style.marginLeft="0";
+  document.getElementById("sidemenu1").style.visibility="hidden";
+ document.getElementById("sidemenu1").style.width="0";
  document.getElementById("get_list").style.visibility="visible";
 }
+ /*
 function openup(){
     document.getElementById("sidemenu").style.width = "100%";
 }
@@ -19,20 +21,29 @@ function closeup(){
   document.getElementById("sidemenu").style.width="0";
   document.getElementById("get_list").style.visibility="visible";
 }
-
+*/
 function windowSize() {
   windowHeight = window.innerHeight ? window.innerHeight : $(window).height();
   windowWidth = window.innerWidth ? window.innerWidth : $(window).width();
 
 }
 windowSize();
-/*
+
 $(window).resize(function() {
   windowSize();
-  if (windowWidth < 768) {
-    closebar();
+  if (windowWidth > 768) {
+    //document.getElementById("sidemenu1").style.display="none";
+    document.getElementById("sidemenu1").style.visibility="hidden";
+    document.getElementById("get_list").style.visibility="hidden";
+    document.getElementById("sidemenu1").style.width="0px";
+    document.getElementById("sidemenu1").style.height="0px";
   }
-});
+  else{
+    if(document.getElementById("sidemenu1").style.visibility=="hidden"){
+      document.getElementById("get_list").style.visibility="visible";
+    }
+  }
+});/*
 addEventListener("resize",function(){
   windowSize() ;
   if( windowWidth < 768){
